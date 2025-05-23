dae6b3ed61478896ed0e65aebc88f7e047c65ff6
EngMohamed71 <mf2976527@gmail.com>
2025-05-24 01:59:04 +0300

    Key differences : implemented hashing functions for strings

--- account.cpp
+++ account.cpp
@@ -1,19 +1,13 @@
 #pragma once
 #include <iostream>
 #include "account.hpp"
-using namespace std;
 
-
-// admin::admin(std::string n){
-//     if (lastUid == 1000){
-//         std::cerr<<"Can't have more than 1000 admin\n";
-//     }
-//     else{
-//         uid = lastUid++;
-//         userName = n;
-//     }
-// }
-
-int main(){
-    cout<<int('a');
+admin::admin(std::string n){
+    if (lastUid == 1000){
+        std::cerr<<"Can't have more than 1000 admin\n";
+    }
+    else{
+        uid = lastUid++;
+        userName = n;
+    }
 }
--- hashTable.hpp
+++ hashTable.hpp
@@ -1,10 +1,75 @@
-#include "account.hpp"
+#include <string>
 
 
-int hashFunction(std::string &name){
+/* 
+    #################### first character hashing by me #######################
 
-    if (name.at(0) >= 'a' && name.at(0) <= 'a') // checking if first character is capitalized
+    this hash function takes a string and return an index depending on 
+    the first character, where it is sorted from A to Z for uppercase letters, 
+    and for lowercase letters it continues from index 26 to reduce conflicts providing 
+    better searching effitioncy
+
+    ### Advantages : the table will be sorted by default , KISS applied.
+    ### Disadvantages : Not optimum for applications where some 'first characters' are used
+        more than the others like people names. hash table size should be 52
+        and of course this is not optimum for large databases.
+*/ 
+
+int firstCharacterHashing(std::string &name){
+
+    const char firstCharacter = name.at(0);
+    if (firstCharacter >= 'A' && firstCharacter <= 'Z') // checking if first character is uppercase
     {
-        
+        return int(firstCharacter) - 65 ;
+    }
+    else {
+        return int(firstCharacter) - 71 ; // Start from index 26 for more searching effectioncy
+    }
+}
+
+/*  another better hash function, good for storing names, but not optimum for all strings
+    because it ignors the position of the string, for example ali == lia
+*/
+int sumHashing(std::string &key , int tableSize){
+
+    int sum = 0;
+    for (size_t i = 0; i < key.size()){
+        sum += int(key.at(i));
+    }
+
+    return sum % tableSize;
+}
+
+
+// character position matters, avoids overflow by making i%5
+int weigtedSumHashing(std::string &key , int tableSize){
+
+    unsigned int sum = 0;
+    for (size_t i = 0; i < key.size(); i++){
+        sum += int(key.at(i)) * (i%5 + 1) ;
+    }
+
+    return sum % tableSize;
+}
+
+/*  
+    - exchanges time and memory, hash table will have 52^m objects, where m is the number of characters taken
+    its like sorting the table by the first character, each string will have a hash table sorting the second letter,
+    and so on, you could make it o(1) but this will require a huge ammount of memory, maybe in a search engine.
+    you could also make the memory usage 26^m if you implemented a function to decapitalize your string.
+
+    - keep in mind that you should ignore non alphabitic characters, or the memory usage will be larger.
+
+    - in small projects, you could take 2 or 3 characters.
+*/
+int mazeHashing(std::string &name , int characterPosition){
+
+    const char character = name.at(characterPosition);
+    if (character >= 'A' && character <= 'Z') // checking if the character is uppercase
+    {
+        return int(character) - 65 ;
+    }
+    else {
+        return int(character) - 71 ; // Start from index 26 for more searching effectioncy
     }
 }
