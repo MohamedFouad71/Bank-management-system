54ed49c24de45afd8fac480905b142c1c219a52f
EngMohamed71 <mf2976527@gmail.com>
2025-05-26 22:34:50 +0300

    Rearranged Files, Changed it Into a Cmake Project

---
+++ build/CMakeFiles/BankApp.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/My_GitHub_Repos/Bank-Management-System/Bank-management-system/src/account.cpp" "CMakeFiles/BankApp.dir/src/account.cpp.obj" "gcc" "CMakeFiles/BankApp.dir/src/account.cpp.obj.d"
+  "D:/My_GitHub_Repos/Bank-Management-System/Bank-management-system/src/functions.cpp" "CMakeFiles/BankApp.dir/src/functions.cpp.obj" "gcc" "CMakeFiles/BankApp.dir/src/functions.cpp.obj.d"
+  "D:/My_GitHub_Repos/Bank-Management-System/Bank-management-system/src/main.cpp" "CMakeFiles/BankApp.dir/src/main.cpp.obj" "gcc" "CMakeFiles/BankApp.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BankApp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "BankApp.exe"
+  "BankApp.exe.manifest"
+  "BankApp.pdb"
+  "CMakeFiles/BankApp.dir/src/account.cpp.obj"
+  "CMakeFiles/BankApp.dir/src/account.cpp.obj.d"
+  "CMakeFiles/BankApp.dir/src/functions.cpp.obj"
+  "CMakeFiles/BankApp.dir/src/functions.cpp.obj.d"
+  "CMakeFiles/BankApp.dir/src/main.cpp.obj"
+  "CMakeFiles/BankApp.dir/src/main.cpp.obj.d"
+  "libBankApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BankApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
